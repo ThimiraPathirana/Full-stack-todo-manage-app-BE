faea5ed3ab4e8ecdc983a559b252f2babca52e21
ThimiraPathirana <thimira.pathirana@gmail.com>
2024-08-28 21:16:30 +0530

    first commit

---
+++ src/test/java/com/fullstack/jwt/FullStackJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
