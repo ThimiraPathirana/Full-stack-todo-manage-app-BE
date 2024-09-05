2a195d150f1777631bc6f5255bd8e076d2ee01c8
ThimiraPathirana <thimira.pathirana@gmail.com>
2024-09-06 00:21:37 +0530

    implement csrf-token

--- src/main/java/com/fullstack/jwt/controller/EventController.java
+++ src/main/java/com/fullstack/jwt/controller/EventController.java
@@ -1,14 +1,34 @@
 package com.fullstack.jwt.controller;
 
+import com.fullstack.jwt.dto.EventTypeDto;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class EventController {
 
+    private List<EventTypeDto> typeDtos = new ArrayList<>(List.of(
+            new EventTypeDto(1, "Music", "Musical Event"),
+            new EventTypeDto(2, "Meetup", "Educationl Meetup")
+    ));
+
     @GetMapping("/")
     public String HelloEvent(HttpServletRequest request) {
         return "Welcome first event! " + request.getSession().getId();
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getSession().getAttribute("_csrf");
+    }
+
+    @GetMapping("/event")
+    public List<EventTypeDto> getEventTypes(HttpServletRequest request) {
+        return typeDtos;
     }
 }
--- src/main/java/com/fullstack/jwt/dto/EventTypeDto.java
+++ src/main/java/com/fullstack/jwt/dto/EventTypeDto.java
@@ -1,4 +1,19 @@
 package com.fullstack.jwt.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EventTypeDto {
+
+    Integer id;
+    String name;
+    String description;
+
+    public EventTypeDto(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
