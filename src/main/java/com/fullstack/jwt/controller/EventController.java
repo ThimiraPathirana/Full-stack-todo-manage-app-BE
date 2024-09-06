d99c6268c300f11db3d28260159093d995d614e2
ThimiraPathirana <thimira.pathirana@gmail.com>
2024-09-06 20:56:55 +0530

    implement add event using csrf-token

--- src/main/java/com/fullstack/jwt/controller/EventController.java
+++ src/main/java/com/fullstack/jwt/controller/EventController.java
@@ -4,6 +4,7 @@ import com.fullstack.jwt.dto.EventTypeDto;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class EventController {
 
     private List<EventTypeDto> typeDtos = new ArrayList<>(List.of(
             new EventTypeDto(1, "Music", "Musical Event"),
-            new EventTypeDto(2, "Meetup", "Educationl Meetup")
+            new EventTypeDto(2, "Meetup", "Educational Meetup")
     ));
 
     @GetMapping("/")
@@ -24,11 +25,17 @@ public class EventController {
 
     @GetMapping("/csrf-token")
     public CsrfToken getCsrfToken(HttpServletRequest request) {
-        return (CsrfToken) request.getSession().getAttribute("_csrf");
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 
     @GetMapping("/event")
     public List<EventTypeDto> getEventTypes(HttpServletRequest request) {
+        return typeDtos;
+    }
+
+    @PostMapping("/event/add")
+    public List<EventTypeDto> addEventType(EventTypeDto eventTypeDto) {
+        typeDtos.add(eventTypeDto);
         return typeDtos;
     }
 }
